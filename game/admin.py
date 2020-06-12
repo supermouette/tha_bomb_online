4155f4601e0d1622467cc5fd610a35646dc3ec1d
Loïc Souverain <loic.souverain@gmail.com>
2020-06-12 17:49:11 +0200

    add admin action to delete unused cards

--- game/admin.py
+++ game/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Game, Player, Card, Sky
 
+
+def clear_card_from_finished_games():
+    Card.objects.filter(game__status__in=[Game.BLUE_WIN, Game.RED_WIN]).delete()
+
+
 admin.site.register(Game)
 admin.site.register(Player)
 admin.site.register(Card)
 admin.site.register(Sky)
+admin.site.add_action(clear_card_from_finished_games)
