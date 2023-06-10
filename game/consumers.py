85fb56d2fef139f213819bd080ca07ea8d0a04be
Loïc Souverain <loic.souverain@gmail.com>
2023-06-10 11:23:36 +0200

    try clicker in ram

--- game/consumers.py
+++ game/consumers.py
@@ -7,13 +7,21 @@ from game.models import Clicker, ClickerReward
 
 
 class ClickerConsumer(WebsocketConsumer):
+
+    rooms = {}
+    rewards = ClickerReward.objects.order_by("threshold").all()
+
     def connect(self):
         self.room = self.scope["url_route"]["kwargs"]["room"]
         self.room_group = "chat_%s" % self.room
 
-        room = get_object_or_404(Clicker, name=self.room)
-        room.active_player += 1
-        room.save()
+        if self.room in ClickerConsumer.rooms:
+            room = ClickerConsumer.rooms[self.room]
+        else:
+            room = {"total": 0, "active_player": 0}
+            ClickerConsumer.rooms[self.room] = room
+            
+        room["active_player"] += 1
 
         async_to_sync(self.channel_layer.group_add)(self.room_group, self.channel_name)
 
@@ -21,20 +29,24 @@ class ClickerConsumer(WebsocketConsumer):
             self.room_group,
             {
                 "type": "clicker_player_up",
-                "total": room.active_player,
+                "total": room["active_player"],
             },
         )
 
         self.accept()
 
     def disconnect(self, close_code):
-        room = get_object_or_404(Clicker, name=self.room)
-        room.active_player -= 1
-        room.save()
+
+        if self.room in ClickerConsumer.rooms:
+            room = ClickerConsumer.rooms[self.room]
+        else:
+            raise ValueError("roomp does not exist")
+        
+        room["active_player"] -= 1
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group,
-            {"type": "clicker_player_down", "total": room.active_player},
+            {"type": "clicker_player_down", "total": room["active_player"]},
         )
 
         async_to_sync(self.channel_layer.group_discard)(
@@ -44,30 +56,38 @@ class ClickerConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         options = json.loads(text_data)
-        room = get_object_or_404(Clicker, name=self.room)
-        active_rewards = room.active_rewards()
-        click_power_upgrade = active_rewards.filter(effect_type="click_power").order_by(
-            "-threshold"
-        ).all()
+
+        if self.room in ClickerConsumer.rooms:
+            room = ClickerConsumer.rooms[self.room]
+        else:
+            raise ValueError("roomp does not exist")
+
+        active_rewards = [r for r in ClickerConsumer.rewards if r.threshold<= room["total"]]
+
+        click_power_upgrade = [r for r in active_rewards if r.effect_type=="click_power"]
+
         click_power = 0
         if len(click_power_upgrade) == 0:
             click_power = 1
-        elif click_power_upgrade[0].name == "Teamwork":
-            click_power = room.active_player
-        elif click_power_upgrade[0].name == "contributor must be rewarded":
-            click_power = room.active_player * max(1, options["contrib"])
+        elif click_power_upgrade[-1].name == "Teamwork":
+            click_power = room["active_player"]
+        elif click_power_upgrade[-1].name == "contributor must be rewarded":
+            click_power = room["active_player"] * max(1, options["contrib"])
 
-        room.total += click_power
-        room.save()
-        active_rewards = room.active_rewards()
+        room["total"] += click_power
+
+        active_rewards = [r for r in ClickerConsumer.rewards if r.threshold<= room["total"]]
+
+
+
         async_to_sync(self.channel_layer.group_send)(
             self.room_group,
             {
                 "type": "clicker_total_up",
-                "total": room.total,
+                "total": room["total"],
                 "options": options,
-                "active_rewards": [r.name for r in active_rewards.only("name").all()],
-                "next_reward": room.next_reward(),
+                "active_rewards": [r.name for r in active_rewards],
+                "next_reward": None if len(ClickerConsumer.rewards) == len(active_rewards) else ClickerConsumer.rewards[len(active_rewards)].threshold,
                 "click_power": click_power,
             },
         )
--- game/views/clicker.py
+++ game/views/clicker.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
+from game.consumers import ClickerConsumer
 from game.models import Clicker
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.urls import reverse
 
 def room_page(request, room_name):
-    room = Clicker.objects.filter(name=room_name).first()
-    if not room : 
-        room = Clicker(name=room_name)
-        room.save()
-
+    
+    if room_name in ClickerConsumer.rooms:
+        room = ClickerConsumer.rooms[room_name]
+    else:
+        room = {"total": 0, "active_player": 1}
+    
+    active_rewards = [r for r in ClickerConsumer.rewards if r.threshold<= room["total"]]
+    print(room)
     return render(
         request,
         "game/clicker.html",
         {
-            "room": room,
-            "active_rewards": room.active_rewards(),
-            "next_reward": room.next_reward(),
+            "room": {**room, "name": room_name},
+            "active_rewards": active_rewards,
+            "next_reward": None if len(ClickerConsumer.rewards) == len(active_rewards) else ClickerConsumer.rewards[len(active_rewards)],
+
         },
     )
